3e16264d86ab2795a1c5e7c9448a33e6989c29ac
Bradley Gohman <bradley.s.gohman@gmail.com>
2015-07-30 16:49:46 -0500

    updates for recieving client side javascript

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -2,11 +2,8 @@ class API::EventsController < ApplicationController
   skip_before_action :verify_authenticity_token
   before_filter :set_access_control_headers
 
-  def set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-    headers['Access-Control-Allow-Headers'] = 'Content-Type'
-  end
+
+
 
   def create
     @registered_application = RegisteredApplication.find_by(url: request.env['HTTP_ORIGIN'])
@@ -23,6 +20,12 @@ class API::EventsController < ApplicationController
   private
     def event_params
       params.permit(:name)
+    end
+
+    def set_access_control_headers
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+      headers['Access-Control-Allow-Headers'] = 'Content-Type'
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,11 @@ Rails.application.routes.draw do
   resources :registered_applications
 
   namespace :api, defaults: { format: :json } do
-    match 'create_event', to: 'events#create', via: [:opions]
+    match 'create_event', to: 'events#create', via: [:options]
     resources :events, only: [:create]
   end
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
